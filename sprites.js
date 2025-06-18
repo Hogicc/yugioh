702b4201167239264be2b49f30b5fef1b4659633
Hogicc <vukasovictomo309@gmail.com>
2025-06-18 14:38:24 +0300

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -1,44 +1,49 @@
+// Golub (klasičan)
 function drawPigeon(ctx, x, y, scale = 1) {
   ctx.save();
   ctx.translate(x, y);
   ctx.scale(scale, scale);
-  ctx.fillStyle = "#999";
-  ctx.beginPath();
-  ctx.ellipse(0, 0, 20, 15, 0, 0, Math.PI * 2);
-  ctx.fill();
-  // oči i kljun
-  ctx.fillStyle = "#000";
-  ctx.beginPath();
-  ctx.arc(8, -5, 4, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.fillStyle = "#f90";
-  ctx.beginPath();
-  ctx.moveTo(20, 0);
-  ctx.lineTo(30, 5);
-  ctx.lineTo(20, 10);
-  ctx.fill();
+
+  // telo
+  ctx.fillStyle = "#555";
+  ctx.fillRect(0, 0, 12, 8);
+
+  // glava
+  ctx.fillStyle = "#ccc";
+  ctx.fillRect(3, -4, 6, 6);
+
+  // kljun
+  ctx.fillStyle = "orange";
+  ctx.fillRect(9, -1, 3, 2);
+
   ctx.restore();
 }
 
+// Mrvica hleba
 function drawBreadcrumb(ctx, x, y, scale = 1) {
   ctx.save();
   ctx.translate(x, y);
   ctx.scale(scale, scale);
-  ctx.fillStyle = "#f5e06e";
-  ctx.beginPath();
-  ctx.arc(0, 0, 6, 0, Math.PI * 2);
-  ctx.fill();
+
+  ctx.fillStyle = "#f4d35e"; // boja hleba
+  ctx.fillRect(0, 0, 6, 4);
+  ctx.fillRect(1, -1, 4, 2); // "zaobljen" vrh
+
   ctx.restore();
 }
 
+// Priča (prepreka)
 function drawStory(ctx, x, y, scale = 1) {
   ctx.save();
   ctx.translate(x, y);
   ctx.scale(scale, scale);
+
   ctx.fillStyle = "#d63031";
   ctx.fillRect(0, 0, 20, 20);
+
   ctx.fillStyle = "#fff";
   ctx.font = "16px Arial";
   ctx.fillText("P", 5, 16);
+
   ctx.restore();
 }
